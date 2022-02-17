f9174daf7b4c238952f0b97685a12a0e5f19aa35
DAI <senseye935@gmail.com>
2022-02-17 17:29:02 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -9,6 +9,15 @@ const adminServices = {
     })
       .then(restaurants => cb(null, { restaurants }))
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(deleteRestaurant => cb(null, { restaurant: deleteRestaurant }))
+      .catch(err => cb(err))
   }
 }
 
